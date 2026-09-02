c44f27bd3e8d58c239f3ae1f08ef6ce0f5c21b2b
Andrew Wallace <japawallace16@gmail.com>
2026-09-02 07:05:55 +0700

    chore: init

---
+++ Qualverum/QualverumApp.swift
@@ -0,0 +1,17 @@
+//
+//  QualverumApp.swift
+//  Qualverum
+//
+//  Created by Andrew Wallace on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct QualverumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
